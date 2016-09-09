6b3dc3e150b3b9048bf1fff12faa29d2ef144295
laurenkl96 <lklynch@ksu.edu>
2016-09-08 22:35:11 -0500

    Update snake.js
    Couldn't figure out what I did wrong

--- snake.js
+++ snake.js
@@ -1,4 +1,3 @@
-/* Global variables */
 var frontBuffer = document.getElementById('snake');
 var frontCtx = frontBuffer.getContext('2d');
 var backBuffer = document.createElement('canvas');
@@ -7,6 +6,16 @@ backBuffer.height = frontBuffer.height;
 var backCtx = backBuffer.getContext('2d');
 var oldTime = performance.now();
 
+var cw= backCtx.width;
+var ch= backCtx.height;
+var apple;
+var apples = [];
+var lastApple = 0;
+var snake_array=[];
+var m;
+
+
+make_snake();
 /**
  * @function loop
  * The main game loop.
@@ -25,6 +34,14 @@ function loop(newTime) {
   window.requestAnimationFrame(loop);
 }
 
+function make_snake(){
+  for(var i = 4; i>=0; i --){
+    var snake= {
+      x:i, y:0,
+    }
+    snake_array.push(snake);
+  }
+}
 /**
  * @function update
  * Updates the game state, moving
@@ -36,15 +53,56 @@ function loop(newTime) {
 function update(elapsedTime) {
 
   // TODO: Spawn an apple periodically
-  // TODO: Grow the snake periodically
+  var a = "red";
+  var apple = { 
+      x: ((Math.random() * cw)/10),
+      y: ((Math.random() * ch)/10),
+    }
+    apples.push(apple);
+  if(elapsedTime > (lastApple + 1500)){
+    lastApple = elapsedTime;
+  }
+  
   // TODO: Move the snake
+  // hdx & hdy are the new head coordinates
+  var hdx = snake_array[0].x;
+  var hdy = snake_array[0].y;
+  if(m == "right")hdx++;
+  else if(m == "left")hdx--;
+  else if(m == "up")hdy--;
+  else if(m == "down")hdy++;
+  var head= snake_array.pop();
+  head.x=hdx;
+  head.y=hdy;
+  snake_array.unshift(head);// puts the head back
   // TODO: Determine if the snake has moved out-of-bounds (offscreen)
-  // TODO: Determine if the snake has eaten an apple
+  if(hdx == -1 || hdx == cw/10 || hdy == -1 || hdy ==ch/10){
+    window.requestAnimationFrame(loop); // restart
+  }
   // TODO: Determine if the snake has eaten its tail
-  // TODO: [Extra Credit] Determine if the snake has run into an obstacle
+  for(var c=0; c<snake_array.length;c++){
+    if(snake_array[c].x==hdx && snake_array[c].y == hdy){
+      window.requestAnimationFrame(loop);//restart
+    }
+  }
+  // TODO: Determine if the snake has eaten an apple
+  // TODO: Grow the snake periodically
+  if(hdx==apple.x && hdy == apple.y){
+    var head={x: hdx, y: hdy}
+  }
+  
+
+  
 
 }
-
+document.addEventListener("keypress", keydown);
+function keydown(e){
+  var key = e.which;
+  if(key == "37" && m!="right")m="left";
+  else if(key == "38" && m!="down")m="up";
+  else if(key == "39" && m!="left")m="right";
+  else if(key == "38" && m!="up")m="down";
+}
 /**
   * @function render
   * Renders the current game state into a back buffer.
@@ -55,7 +113,15 @@ function render(elapsedTime) {
   backCtx.clearRect(0, 0, backBuffer.width, backBuffer.height);
 
   // TODO: Draw the game objects into the backBuffer
-
+  //Apple drawings
+  var appl = apples.pop();
+  backCtx.fillStyle="red";
+  backCtx.fillRect(appl.x,appl.y,10,10);
+  //Snake drawing
+  for(var b = 0; b < snake_array.length; b++){
+    backCtx.fillStyle="olive";
+    backCtx.fillRect(snake_array[b].x,snake_array[b].y, 10, 10);
+  }
 }
 
 /* Launch the game */
